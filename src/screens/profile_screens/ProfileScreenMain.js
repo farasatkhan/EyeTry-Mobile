53cea0fd99bff2d26776e276852ae7181df57e45
Abdul Sammi Gul <sammigul11005@gmail.com>
2023-05-21 15:50:08 +0500

    fixed condition where no user image is present or when user has deleted the image

--- src/screens/profile_screens/ProfileScreenMain.js
+++ src/screens/profile_screens/ProfileScreenMain.js
@@ -1,16 +1,26 @@
 import * as React from 'react';
-import { CommonActions } from '@react-navigation/native';
+import { CommonActions,useIsFocused } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { logoutUser } from '../../api/authapi';
+
+import { uploadImageToServer, viewProfileImage } from '../../api/userapi';
+
 // Importing UI Components
 import Container from '../../components/ui/Container';
 import ProfileItem from '../../components/ui/ProfileItem';
 import ImageWithText from '../../components/ui/ImageWithText';
 import { ScrollView, } from 'react-native';
+import { storeDataAsyncStorage } from '../../utils/asyncStorage';
 
 
 
 export default function ProfileScreenMain({navigation}) {
+  
+  const serverURL = 'http://localhost:3000'; 
+  const [name,setName] = React.useState(null)
+  const [img,setImg] = React.useState(null)
+
+  const isFocused = useIsFocused()
 
 
   // Methods
@@ -33,33 +43,39 @@ export default function ProfileScreenMain({navigation}) {
   }
   
 
-  const [name,setName] = React.useState(null)
-
   React.useEffect( ()=>{
     getDataFromAsyncStorage = async () => {
       try{
         let user = await AsyncStorage.getItem('user')
         user = JSON.parse(user)
-        setName(user.firstName)
 
-        
-        console.log("Checking for first Name")
-        console.log(user.firstName)
-      }
+        const img = await viewProfileImage();
+        setImg(serverURL+img.location)
+        setName(user.firstName)
+        }
       catch (e){
+        if (e.response.status == 400){
+          console.log('No Image is present')
+          setImg(null)
+        }
         console.error(e)
       }
     }
 
     getDataFromAsyncStorage();
-  },[])
+  },[isFocused])
 
 
     return (
       name && (<Container >
         <ScrollView contentContainerStyle={{paddingHorizontal:'3%',flexDirection:'column',flex:1,justifyContent:'space-evenly'
         }}>
-            <ImageWithText name={name} onPress={handleUserImage}/>
+          {
+          img && 
+            <ImageWithText name={name} onPress={handleUserImage} imageSource={img}/>
+        }{
+          !img && <ImageWithText name={name} onPress={handleUserImage}/>
+        }
             <ProfileItem iconName={'md-cart'} name={'My Orders'} iconSize={24} />
             <ProfileItem iconName={'person-outline'} name={'My Details'} iconSize={24} onPress={goToMyDetails}/>
             <ProfileItem iconName={'ios-newspaper-outline'} name={'My Prescriptions'} iconSize={24} onPress={goToMyPrescriptions}/>
