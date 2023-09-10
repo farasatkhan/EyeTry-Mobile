94064c00f3ddeedaa2fd0158074e7c5e3fe26855
Abdul Sammi Gul <sammigul11005@gmail.com>
2023-09-10 11:58:59 +0500

    added Address Screens

--- src/screens/profile_screens/ProfileStackScreen.js
+++ src/screens/profile_screens/ProfileStackScreen.js
@@ -48,33 +48,33 @@ export default function ProfileStackScreen() {
     return (
       <ProfileStack.Navigator screenOptions={{headerTitleAlign:'center'}} initialRouteName='ProfileScreenMain'>
         <ProfileStack.Screen name='ProfileScreenMain' component={ProfileScreenMain} options={{headerShown:false}}/>
-        <ProfileStack.Screen name='MyDetails' component={MyDetails} />
-        <ProfileStack.Screen name='EditDetails' component={EditDetails} />
-        <ProfileStack.Screen name='DeleteAccount' component={DeleteAccount} />
-        <ProfileStack.Screen name='ChangePassword' component={ChangePassword} />
-        <ProfileStack.Screen name='PrescriptionsList' component={PrescriptionsList} />
-        <ProfileStack.Screen name='PrescriptionDetails' component={PrescriptionDetails}  />
-        <ProfileStack.Screen name='EditPrescription' component={EditPrescription} />
-        <ProfileStack.Screen name='EditPrescription2' component={EditPrescription2} />
-        <ProfileStack.Screen name='AddPrescription' component={AddPrescription} />
-        <ProfileStack.Screen name='AddPrescription2' component={AddPrescription2} />
-        <ProfileStack.Screen name='AddressBook' component={AddressBook} />
+        <ProfileStack.Screen name='MyDetails' component={MyDetails} options={{title:"My Details"}}/>
+        <ProfileStack.Screen name='EditDetails' component={EditDetails} options={{title:"Edit Details"}}/>
+        <ProfileStack.Screen name='DeleteAccount' component={DeleteAccount} options={{title:"Delete Account"}}/>
+        <ProfileStack.Screen name='ChangePassword' component={ChangePassword} options={{title:"Change Password"}}/>
+        <ProfileStack.Screen name='PrescriptionsList' component={PrescriptionsList} options={{title:"Prescriptions"}}/>
+        <ProfileStack.Screen name='PrescriptionDetails' component={PrescriptionDetails} options={{title:"Prescription Details"}} />
+        <ProfileStack.Screen name='EditPrescription' component={EditPrescription} options={{title:"Edit Prescription"}}/>
+        <ProfileStack.Screen name='EditPrescription2' component={EditPrescription2} options={{title:"Edit Prescription"}}/>
+        <ProfileStack.Screen name='AddPrescription' component={AddPrescription} options={{title:"Add Prescription"}}/>
+        <ProfileStack.Screen name='AddPrescription2' component={AddPrescription2} options={{title:"Add Prescription"}} />
+        <ProfileStack.Screen name='AddressBook' component={AddressBook} options={{title:"Address Book"}}/>
         <ProfileStack.Screen name='AddAddress' component={AddAddress} options={{title:"Add Address"}}/>
         <ProfileStack.Screen name='AddAddress2' component={AddAddress2} options={{title:"Add Address"}}/>
-        <ProfileStack.Screen name='EditAddress' component={EditAddress} />
-        <ProfileStack.Screen name='EditAddress2' component={EditAddress2} />
-        <ProfileStack.Screen name='PaymentMethods' component={PaymentMethods} />
-        <ProfileStack.Screen name='AddPaymentMethod' component={AddPaymentMethod} />
-        <ProfileStack.Screen name='AddPaymentMethod2' component={AddPaymentMethod2} />
-        <ProfileStack.Screen name='EditPaymentMethod' component={EditPaymentMethod} />
-        <ProfileStack.Screen name='EditPaymentMethod2' component={EditPaymentMethod2} />
-        <ProfileStack.Screen name='TryOnImages' component={TryOnImages} />
-        <ProfileStack.Screen name='UploadTryOnImage' component={UploadTryOnImage} />
-        <ProfileStack.Screen name='EditTryOnImage' component={EditTryOnImage} />
-        <ProfileStack.Screen name='UserImage' component={UserImage} />
-        <ProfileStack.Screen name='GiftCard' component={GiftCard} />
-        <ProfileStack.Screen name='BuyGiftCard' component={BuyGiftCard} />
-        <ProfileStack.Screen name='ManageGiftCard' component={ManageGiftCard} />
+        <ProfileStack.Screen name='EditAddress' component={EditAddress} options={{title:"Edit Address"}}/>
+        <ProfileStack.Screen name='EditAddress2' component={EditAddress2}  options={{title:"Edit Address"}}/>
+        <ProfileStack.Screen name='PaymentMethods' component={PaymentMethods} options={{title:"Payment Methods"}}/>
+        <ProfileStack.Screen name='AddPaymentMethod' component={AddPaymentMethod} options={{title:"Add Payment Method"}}/>
+        <ProfileStack.Screen name='AddPaymentMethod2' component={AddPaymentMethod2} options={{title:"Add Payment Method"}}/>
+        <ProfileStack.Screen name='EditPaymentMethod' component={EditPaymentMethod} options={{title:"Edit Payment Method"}}/>
+        <ProfileStack.Screen name='EditPaymentMethod2' component={EditPaymentMethod2} options={{title:"Add Payment Method"}} />
+        <ProfileStack.Screen name='TryOnImages' component={TryOnImages} options={{title:"Try On Images"}}/>
+        <ProfileStack.Screen name='UploadTryOnImage' component={UploadTryOnImage} options={{title:"Upload Try On Image"}}/>
+        <ProfileStack.Screen name='EditTryOnImage' component={EditTryOnImage} options={{title:"Edit Try On Image"}} />
+        <ProfileStack.Screen name='UserImage' component={UserImage} options={{title:"Profile Image"}}/>
+        <ProfileStack.Screen name='GiftCard' component={GiftCard} options={{title:"Gift Cards"}}/>
+        <ProfileStack.Screen name='BuyGiftCard' component={BuyGiftCard} options={{title:"Buy Gift Cards"}}/>
+        <ProfileStack.Screen name='ManageGiftCard' component={ManageGiftCard} options={{title:"Buy Gift Cards"}}/>
       </ProfileStack.Navigator>
     );
   }
