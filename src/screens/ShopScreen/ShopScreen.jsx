9032368313a472eb486f2c5c3e7708ed86ba25e0
Farasat Khan <farasatkahan@gmail.com>
2023-11-15 21:05:35 +0500

    made minor changes to styles

--- src/screens/ShopScreen/ShopScreen.jsx
+++ src/screens/ShopScreen/ShopScreen.jsx
@@ -15,40 +15,54 @@ const ShopScreen = () => {
   return (
     <SafeAreaView className="bg-white">
       <ScrollView>
-        <Pressable className="flex flex-row justify-between items-center mx-5 mt-5 px-5 py-5 bg-blue-100 rounded-lg shadow-lg shadow-black/40">
-          <Text className="text-black text-base">Eyeglasses</Text>
+        <Pressable className="flex flex-row justify-between items-center mx-5 mt-5 bg-blue-100 rounded-lg shadow-lg shadow-black/40">
+          <View className="pl-5">
+            <Text className="text-black text-xl">Eyeglasses</Text>
+          </View>
           <Image
-            className="w-48 h-48 "
+            className="w-48 h-48 object-cover"
+            resizeMode="contain"
             source={require('../../assets/shop_screen/eyeglasses.png')}
           />
         </Pressable>
-        <Pressable className="flex flex-row justify-between items-center mx-5 mt-5 px-5 py-5 bg-blue-100 rounded-lg shadow-lg shadow-black/40">
-          <Text className="text-black text-base">Sunglasses</Text>
+        <Pressable className="flex flex-row justify-between items-center mx-5 mt-5 bg-blue-100 rounded-lg shadow-lg shadow-black/40">
+          <View className="pl-5">
+            <Text className="text-black text-xl">Sunglasses</Text>
+          </View>
           <Image
-            className="w-48 h-48 "
+            className="w-48 h-48 object-cover"
+            resizeMode="contain"
             source={require('../../assets/shop_screen/sunglasses.png')}
           />
         </Pressable>
         <Pressable
           onPress={() => handleNavigation('VisionAssessmentsHome')}
-          className={`flex flex-row justify-between items-center mx-5 mt-5 px-5 py-5 bg-blue-100 rounded-lg shadow-lg shadow-black/40`}>
-          <Text className="text-black text-base">Vision Assessment</Text>
+          className={`flex flex-row justify-between items-center mx-5 mt-5 bg-blue-100 rounded-lg shadow-lg shadow-black/40`}>
+          <View className="pl-5">
+            <Text className="text-black text-xl">Vision</Text>
+            <Text className="text-black text-xl">Assessment</Text>
+          </View>
           <Image
             className="w-48 h-48 object-cover"
             source={require('../../assets/shop_screen/vision_assessment_2.png')}
           />
         </Pressable>
-        <Pressable className="flex flex-row justify-between items-center mx-5 mt-5 mb-5 px-5 py-5 bg-blue-100 rounded-lg shadow-lg shadow-black/40">
-          <Text className="text-black text-base">IPD</Text>
+        <Pressable className="flex flex-row justify-between items-center mx-5 mt-5 mb-5 bg-blue-100 rounded-lg shadow-lg shadow-black/40">
+          <View className="pl-5">
+            <Text className="text-black text-xl">IPD</Text>
+          </View>
           <Image
-            className="w-48 h-48"
+            className="w-48 h-48 object-cover"
+            resizeMode="contain"
             source={require('../../assets/shop_screen/ipd.png')}
           />
         </Pressable>
-        <Pressable className="flex flex-row justify-between items-center mx-5 mt-5 mb-5 px-5 py-5 bg-blue-100 rounded-lg shadow-lg shadow-black/40">
-          <Text className="text-black text-base">Frame Finder</Text>
+        <Pressable className="flex flex-row justify-between items-center mx-5 mt-5 mb-5 bg-blue-100 rounded-lg shadow-lg shadow-black/40">
+          <View className="pl-5">
+            <Text className="text-black text-xl">Frame Finder</Text>
+          </View>
           <Image
-            className="w-48 h-48"
+            className="w-48 h-48 object-cover"
             source={require('../../assets/shop_screen/frame_finder.png')}
           />
         </Pressable>
