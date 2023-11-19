2878f35ac1f4b3c1919ca22edd0d75395776974b
Farasat Khan <farasatkahan@gmail.com>
2023-11-19 19:27:01 +0500

    added function to request eyeglasses list and sunglasses list

--- src/services/Glasses/Glasses.js
+++ src/services/Glasses/Glasses.js
@@ -10,6 +10,26 @@ export const viewAllGlasses = async () => {
     }
 }
 
+export const viewAllEyeGlasses = async () => {
+    try {
+        const response = await authenticatedAxiosInstance.get('/products/v1/glasses/eyeglasses');
+        return response.data;
+    } catch (error) {
+        console.error('Error while retriving glasses', error);
+        throw error;
+    }
+}
+
+export const viewAllSunGlasses = async () => {
+    try {
+        const response = await authenticatedAxiosInstance.get('/products/v1/glasses/sunglasses');
+        return response.data;
+    } catch (error) {
+        console.error('Error while retriving glasses', error);
+        throw error;
+    }
+}
+
 export const viewSingleProduct = async (productId) => {
     try {
         const response = await authenticatedAxiosInstance.get('/products/v1/glasses/' + productId);
