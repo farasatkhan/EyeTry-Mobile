bea4835dd035fb3c864f06d410b3f49a78fe8a87
Farasat Khan <farasatkahan@gmail.com>
2023-11-25 18:57:11 +0500

    moved images outside from home component

---
+++ src/data/HomeScreenGlassesProperties.js
@@ -0,0 +1,106 @@
+export const raybanImages = [
+  {
+    key: 1,
+    image: require('../assets/home_screen/raybans/image_1.webp'),
+  },
+  {
+    key: 2,
+    image: require('../assets/home_screen/raybans/image_2.webp'),
+  },
+  {
+    key: 3,
+    image: require('../assets/home_screen/raybans/image_3.webp'),
+  },
+  {
+    key: 4,
+    image: require('../assets/home_screen/raybans/image_4.webp'),
+  },
+  {
+    key: 5,
+    image: require('../assets/home_screen/raybans/image_5.webp'),
+  },
+  {
+    key: 6,
+    image: require('../assets/home_screen/raybans/image_6.webp'),
+  },
+];
+
+export const oakleyImages = [
+  {
+    key: 1,
+    image: require('../assets/home_screen/oakley/image_1.webp'),
+  },
+  {
+    key: 2,
+    image: require('../assets/home_screen/oakley/image_2.webp'),
+  },
+  {
+    key: 3,
+    image: require('../assets/home_screen/oakley/image_3.webp'),
+  },
+  {
+    key: 4,
+    image: require('../assets/home_screen/oakley/image_4.webp'),
+  },
+  {
+    key: 5,
+    image: require('../assets/home_screen/oakley/image_5.webp'),
+  },
+  {
+    key: 6,
+    image: require('../assets/home_screen/oakley/image_6.webp'),
+  },
+];
+
+export const exclusiveCollectionImage = [
+  {
+    key: 1,
+    image: require('../assets/home_screen/exclusive/image_1.webp'),
+  },
+  {
+    key: 2,
+    image: require('../assets/home_screen/exclusive/image_2.webp'),
+  },
+  {
+    key: 3,
+    image: require('../assets/home_screen/exclusive/image_3.webp'),
+  },
+  {
+    key: 4,
+    image: require('../assets/home_screen/exclusive/image_3.webp'),
+  },
+];
+
+export const eyewearsCollectionImages = [
+  {
+    key: 1,
+    image: require('../assets/home_screen/eyewears/image_1.webp'),
+  },
+  {
+    key: 2,
+    image: require('../assets/home_screen/eyewears/image_2.webp'),
+  },
+  {
+    key: 3,
+    image: require('../assets/home_screen/eyewears/image_3.webp'),
+  },
+  {
+    key: 4,
+    image: require('../assets/home_screen/eyewears/image_4.webp'),
+  },
+  {
+    key: 5,
+    image: require('../assets/home_screen/eyewears/image_5.webp'),
+  },
+];
+
+export const headerCollection = [
+  {
+    key: 1,
+    image: require('../assets/home_screen/header/image_1.webp'),
+  },
+  {
+    key: 2,
+    image: require('../assets/home_screen/header/image_2.webp'),
+  },
+];
--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -14,112 +14,13 @@ import {useNavigation} from '@react-navigation/native';
 
 import Pressable from '../../wrapper_components/Pressable';
 
-const raybanImages = [
-  {
-    key: 1,
-    image: require('../../assets/home_screen/raybans/image_1.webp'),
-  },
-  {
-    key: 2,
-    image: require('../../assets/home_screen/raybans/image_2.webp'),
-  },
-  {
-    key: 3,
-    image: require('../../assets/home_screen/raybans/image_3.webp'),
-  },
-  {
-    key: 4,
-    image: require('../../assets/home_screen/raybans/image_4.webp'),
-  },
-  {
-    key: 5,
-    image: require('../../assets/home_screen/raybans/image_5.webp'),
-  },
-  {
-    key: 6,
-    image: require('../../assets/home_screen/raybans/image_6.webp'),
-  },
-];
-
-const oakleyImages = [
-  {
-    key: 1,
-    image: require('../../assets/home_screen/oakley/image_1.webp'),
-  },
-  {
-    key: 2,
-    image: require('../../assets/home_screen/oakley/image_2.webp'),
-  },
-  {
-    key: 3,
-    image: require('../../assets/home_screen/oakley/image_3.webp'),
-  },
-  {
-    key: 4,
-    image: require('../../assets/home_screen/oakley/image_4.webp'),
-  },
-  {
-    key: 5,
-    image: require('../../assets/home_screen/oakley/image_5.webp'),
-  },
-  {
-    key: 6,
-    image: require('../../assets/home_screen/oakley/image_6.webp'),
-  },
-];
-
-const exclusiveCollectionImage = [
-  {
-    key: 1,
-    image: require('../../assets/home_screen/exclusive/image_1.webp'),
-  },
-  {
-    key: 2,
-    image: require('../../assets/home_screen/exclusive/image_2.webp'),
-  },
-  {
-    key: 3,
-    image: require('../../assets/home_screen/exclusive/image_3.webp'),
-  },
-  {
-    key: 4,
-    image: require('../../assets/home_screen/exclusive/image_3.webp'),
-  },
-];
-
-const eyewearsCollectionImages = [
-  {
-    key: 1,
-    image: require('../../assets/home_screen/eyewears/image_1.webp'),
-  },
-  {
-    key: 2,
-    image: require('../../assets/home_screen/eyewears/image_2.webp'),
-  },
-  {
-    key: 3,
-    image: require('../../assets/home_screen/eyewears/image_3.webp'),
-  },
-  {
-    key: 4,
-    image: require('../../assets/home_screen/eyewears/image_4.webp'),
-  },
-  {
-    key: 5,
-    image: require('../../assets/home_screen/eyewears/image_5.webp'),
-  },
-];
-
-const headerCollection = [
-  {
-    key: 1,
-    image: require('../../assets/home_screen/header/image_1.webp'),
-  },
-  {
-    key: 2,
-    image: require('../../assets/home_screen/header/image_2.webp'),
-  },
-];
+import {
+  raybanImages,
+  oakleyImages,
+  exclusiveCollectionImage,
+  eyewearsCollectionImages,
+  headerCollection,
+} from '../../data/HomeScreenGlassesProperties';
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
